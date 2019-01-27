25056a074252b7c50bfcb60bd91eeb54f354c5a1
Vincent-Alexandrine <vincent.alexand@gmail.com>
2019-01-27 16:27:01 +0100

    Blueprint first steps: - redux-cli installed globally (see if there a way around, !redux-cli isnt redux exclusive) - first blueprint for component (gonna rename for atomic design)

---
+++ blueprints/component/files/__root__/__component__/__name__/__name__.component.jsx
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class <%= pascalEntityName %> extends PureComponent {
+  static defaulProps = {
+
+  };
+
+  static propTypes = {
+
+  };
+  
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default <%= pascalEntityName %>;
